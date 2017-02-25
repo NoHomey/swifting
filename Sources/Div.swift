ff4845eed803f139247bc70f7286ba5fcd2e9675
NoHomey <ivo.stratev.tues@gmail.com>
2017-02-25 17:48:19 +0200

    spliting into attributes struc

--- Sources/Div.swift
+++ Sources/Div.swift
@@ -1,22 +1,39 @@
-class Div {
-    var value: Observer<String>
-    var subscription: Int = 0
-    var onChange: Int = 0
+struct DivAttributes {
+    var value: Observer<String>? = nil
+    var onChange: ((_ oldValue: String, _ newValue: String) -> Void)? = nil
+    var id: Observer<String>? = nil
 
     init(value: Observer<String>) {
         self.value = value
-        self.subscription = value.subscribe(sync)
     }
 
-    convenience init(value: Observer<String>, onChange: @escaping (_ oldValue: String, _ newValue: String) -> Void) {
-        self.init(value: value)
-        self.onChange = value.subscribe(onChange)
+    init(value: Observer<String>, onChange: @escaping (_ oldValue: String, _ newValue: String) -> Void) {
+        self.value = value
+        self.onChange = onChange
+    }
+}
+
+class Div {
+    private var subscription: Int = 0
+    private var onChange: Int = 0
+    let attributes: DivAttributes
+
+    init(_ attributes: DivAttributes) {
+        self.attributes = attributes
+        if(attributes.value != nil) {
+            self.subscription = attributes.value!.subscribe(sync)
+            if(attributes.onChange != nil) {
+                self.onChange = attributes.value!.subscribe(attributes.onChange!)
+            }
+        }
     }
 
     deinit {
-        value.unsubscribe(id: subscription)
-        if(onChange != 0) {
-            value.unsubscribe(id: onChange)
+        if(self.attributes.value != nil) {
+            self.attributes.value!.unsubscribe(id: subscription)
+            if(onChange != 0) {
+                self.attributes.value!.unsubscribe(id: onChange)
+            }
         }
     }
 
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,12 +1,12 @@
 var userData = User(email: "ivo@ivo.ovi", password: "ivoovi")
 var user = UserStore(userData)
-let div1 = Div(
+let div1 = Div(DivAttributes(
     value: user.email,
     onChange: {(oldValue: String, newValue: String) in
         print("email: ", newValue)
     }
-)
-let div2 = Div(value: user.password)
+))
+let div2 = Div(DivAttributes(value: user.password))
 user.email.obj = "swift@linux"
 userData.password = "oviivo"
 user.update(userData)
